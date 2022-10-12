395c75022f3d14954e81c938701baf44ba98b8b9
Lance Hentges <Lance.Hentges@gmail.com>
2022-10-12 12:34:25 +0100

    Add function to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let reverseString = ''
+    for (i=inputString.length; i>0; i--){
+        reverseString+=inputString[i-1]
+    }
+    return reverseString
 };
 
 // Do not edit below this line
