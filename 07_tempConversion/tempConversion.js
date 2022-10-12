a195510061ca632ea7e049066200667660548c42
Lance Hentges <Lance.Hentges@gmail.com>
2022-10-12 15:36:24 +0100

    Convert output from string to float

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const ftoc = function(tempF) {
   let tempC = (tempF-32)/1.8
-  return tempC.toFixed(1)
+  return parseFloat(tempC.toFixed(1))
 };
 
 const ctof = function(tempC) {
   let tempF = (tempC*1.8)+32
-  return tempF.toFixed(1)
+  return parseFloat(tempF.toFixed(1))
 };
 
 // Do not edit below this line
