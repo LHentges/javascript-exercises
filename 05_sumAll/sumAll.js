bba7b9d2b2b54f51e7c0581a3e8710e269144711
Lance Hentges <Lance.Hentges@gmail.com>
2022-10-12 13:27:14 +0100

    Add summation loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, lastNum) {
+    returnSum
+    for (let i=startNum; i<=startNum+lastNum; i++){
+        returnSum+=i
+    }
+    return returnSum
 };
 
 // Do not edit below this line
