89598c2548e4f73308a5af6939f57ef818f233f1
Lance Hentges <Lance.Hentges@gmail.com>
2022-10-12 12:21:43 +0100

    Add loop for concatenating string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    returnString = ''
+    for (let i=0; i<num; i++){
+        returnString+=string
+    }
 };
 
 // Do not edit below this line
