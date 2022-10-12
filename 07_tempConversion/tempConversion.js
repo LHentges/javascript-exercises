7fcee01f3e792c08f36d6383e9a08e9c288d84a1
Lance Hentges <Lance.Hentges@gmail.com>
2022-10-12 15:34:10 +0100

    Add formula for temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempC = (tempF-32)/1.8
+  return tempC.toFixed(1)
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = (tempC*1.8)+32
+  return tempF.toFixed(1)
 };
 
 // Do not edit below this line
