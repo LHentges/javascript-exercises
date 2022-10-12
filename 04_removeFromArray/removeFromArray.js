d6d964eb9631d096dd454dde1a013a614cf5c5aa
Lance Hentges <Lance.Hentges@gmail.com>
2022-10-12 13:19:57 +0100

    Remove matching elements using splice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...args) {
+    for (const arg of args){
+        index = array.indexOf(arg)
+        if (index!==-1){
+            inputArray.splice(index, 1)
+        }
+    }
+    return inputArray
 };
 
 // Do not edit below this line
