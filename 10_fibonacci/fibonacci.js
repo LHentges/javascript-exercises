53f84e69d08d47a5732fbb72652f2f0d5152b4cd
Lance Hentges <Lance.Hentges@gmail.com>
2023-01-19 10:27:18 +0000

    Add fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibNum) {
+    if (fibNum <= 0){
+        return "OOPS"
+    }
+    let prevStepOne = 1
+    let prevStepTwo = 1
+    let currentFib = 1
+    if (fibNum != 1 && fibNum != 2){
+        for (let i = 2; i < fibNum; i++){
+            currentFib = prevStepOne+prevStepTwo
+            prevStepTwo = prevStepOne
+            prevStepOne = currentFib
+        }
+    }
+    return currentFib    
 };
 
 // Do not edit below this line
