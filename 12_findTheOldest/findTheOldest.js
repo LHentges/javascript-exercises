e28be0f2305f951e1de8e3610101c8aaef78d37a
Lance Hentges <Lance.Hentges@gmail.com>
2023-01-19 11:01:40 +0000

    Add function to find the oldest from array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldestPerson = {}
+    array.reduce(function(currentOldAge, arrayItem){
+        if (!arrayItem.hasOwnProperty('yearOfDeath')){
+            const d = new Date();
+            arrayItem.yearOfDeath = d.getFullYear(); 
+        }
+        let personAge = arrayItem.yearOfDeath - arrayItem.yearOfBirth
+        if (personAge > currentOldAge){
+            oldestPerson = arrayItem
+            currentOldAge = personAge
+        }
+        return currentOldAge
+    }, 0)
+    return oldestPerson
 };
 
 // Do not edit below this line
