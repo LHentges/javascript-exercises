7154b84fabf6355e42461462418fd58b701bcbb3
Lance Hentges <Lance.Hentges@gmail.com>
2023-01-19 09:56:53 +0000

    Add palindrom function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+let cleanString = string.toLowerCase().replace(/[^a-z]/g, "")
+let revCleanString = cleanString.split("").reverse().join("")
+return cleanString == revCleanString
+}
 
 // Do not edit below this line
 module.exports = palindromes;
